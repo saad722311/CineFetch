770d1ff11901fde940307b721ef686aef858b335
saad722311 <145096490+saad722311@users.noreply.github.com>
2025-04-01 17:03:48 +0400

    create a navbar component
    added navbar component, which wraps to the whole app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
-import MovieCard from './components/MovieCard'
-import Home from './pages/Home'
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <NavBar />
+      <div>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
